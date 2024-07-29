ec503d3534d4948f5055aaf9657ffbbfd98d3db3
Narcisse <narcissekabongongandu@gmail.com>
2024-07-29 18:49:36 +0300

    Another commit

--- main.py
+++ main.py
@@ -1,28 +1,51 @@
 from tkinter import *
 from tkinter import messagebox
-
+from random import choice, randint, shuffle
+import pyperclip
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
-# ---------------------------- SAVE PASSWORD ------------------------------- #
-def add_password():
-    website = web_site_entry.get()
-    email = mail_entry.get()
-    password = password_entry.get()
+#Password Generator Project
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-    is_ok = messagebox.askokcancel(title=website, message=f"These are "
-                                                          f"the details entered: \nEmail: "
-                                                          f"{email}\nPassword: {password}\nIs it"
-                                                          f" ok to save?")
-    if is_ok:
-        with open("data.txt", "a") as file:
-            file.write(f"{website} | {email} | {password}\n")
-            web_site_entry.delete(0, END)
-            password_entry.delete(0, END)
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
-        messagebox.showinfo(title="Cancelled", message="Your password was not saved")
-        web_site_entry.delete(0, END)
-        password_entry.delete(0, END)
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
+                                                      f"\nPassword: {password} \nIs it ok to save?")
+        if is_ok:
+            with open("data.json", "a") as data_file:
+                json.dump(new_data, data_file) 
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -31,33 +54,33 @@ window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
 
-image = PhotoImage(file="logo.png")
-canvas = Canvas(width=200, height=200)
-canvas.create_image(60, 100, image=image)
+canvas = Canvas(height=200, width=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
-# Labels
-web_site_label = Label(text="Website:", font=("Arial", 12, "bold"))
-web_site_label.grid(row=1, column=0)
-mail_label = Label(text="Email/Username:", font=("Arial", 12, "bold"))
-mail_label.grid(row=2, column=0)
-password_label = Label(text="Password:", font=("Arial", 12, "bold"))
+#Labels
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
-# Entries
-web_site_entry = Entry(width=35)
-web_site_entry.grid(row=1, column=1, columnspan=2)
-web_site_entry.focus()
-mail_entry = Entry(width=35)
-mail_entry.grid(row=2, column=1, columnspan=2)
-mail_entry.insert(0, "narcissekabongongandu@gmail.com")
-password_entry = Entry(width=35)
-password_entry.grid(row=3, column=1, columnspan=2)
+#Entries
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "narcissekabongongandu@gmail.com")
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
 
 # Buttons
-generate_button = Button(text="Generate Password", font=("Arial", 11, "bold"), width=23)
-generate_button.grid(row=4, column=1, columnspan=2)
-add_button = Button(text="Add", font=("Arial", 11, "bold"), width=23, command=add_password)
-add_button.grid(row=5, column=1, columnspan=2)
+generate_password_button = Button(text="Generate Password", command=generate_password)
+generate_password_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=36, command=save)
+add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
